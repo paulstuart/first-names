a043f56f6d5e2a0eafc3ef94287d17be1061bfc2
Paul Stuart <pauleyphonic@gmail.com>
2025-12-05 22:39:54 -0800

    fix load script to work with the actual included data file

--- load.sh
+++ load.sh
@@ -16,10 +16,24 @@ DIR=${DIR:-names}
 rm -f "$DB"
 sqlite3 "$DB" < schema.sql
 
-./prepend_year.sh ${DIR} | sqlite3 ${DB} -cmd ".mode csv" ".import /dev/stdin first_names"
+loadssa() {
+  ./prepend_year.sh "${DIR}" | sqlite3 "${DB}" -cmd ".mode csv" ".import /dev/stdin first_names"
+}
 
-sqlite3 ${DB} <<EOF
+loadpre() {
+  zstdcat all_years.csv.zst | sqlite3 "${DB}" -cmd ".mode csv" ".import /dev/stdin first_names"
+}
+
+if [[ -d ${DIR} ]]
+then
+    loadssa
+else
+    loadpre
+fi
+
+sqlite3 "${DB}" <<EOF
 -- for some reason the view had an empty first_name entry, despite not seeing it in the data
 create view v_names as select distinct first_name from first_names where first_name > '';
 create view v_totals as select first_name, sum(count) as total from first_names group by first_name;
+create view v_gendered as select first_name, gender, sum(count) as total from first_names group by first_name, gender;
 EOF
